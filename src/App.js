af1cba86983a7c3840fc4f169128d0a8ffe88487
JumaKenn <jumaliko711@gmail.com>
2023-05-18 22:25:20 +0300

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [imageURL, setImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const generateImage = () => {
+    setIsLoading(true); // Set loading state to true
+
+    // Replace 'YOUR_API_ENDPOINT' with the actual endpoint that generates the image
+    fetch('https://django-server-production-602e.up.railway.app/apis/generate-image')
+      .then(response => response.json())
+      .then(data => {
+        // Assuming the API response returns the image URL
+        setImageURL(data.imageURL);
+        setIsLoading(false); // Set loading state to false after receiving the response
+      })
+      .catch(error => {
+        console.log(error);
+        setIsLoading(false); // Set loading state to false in case of an error
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Image Generator App</h1>
+      <button onClick={generateImage}>Generate Image</button>
+      {isLoading ? (
+        <div className="progress-bar">Loading...</div>
+      ) : (
+        imageURL && <img src={imageURL} alt="Generated Image" />
+      )}
     </div>
   );
 }
