960f6710ec88c2db15d47cd3d34fce6f8b0a856c
JumaKenn <jumaliko711@gmail.com>
2023-05-18 23:49:51 +0300

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import axios from 'axios';
 function App() {
   const [imageURL, setImageURL] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [imageSrc, setImageSrc] = useState('');
 
   useEffect(() => {
     generateImage();
   }, []);
 
- 
-
   const generateImage = async () => {
     setIsLoading(true); // Set loading state to true
     try {
-      const response = await axios.get('https://django-server-production-602e.up.railway.app/apis/generate-image');
-      setImageSrc(response.data);
-      setIsLoading(false); // Set loading state to false after receiving the response
-      } catch(error) {
-        console.log(error);
-        setIsLoading(false); // Set loading state to false in case of an error
-      }
+      await axios.get('https://django-server-production-602e.up.railway.app/apis/generate-image', {
+        responseType: 'blob'
+      })
+      .then(response => {
+        const blob = new Blob([response.data], { type: 'image/jpeg' });
+        const imageURL = URL.createObjectURL(blob);
+        setImageURL(imageURL);
+        setIsLoading(false); // Set loading state to false after receiving the response
+      });
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false); // Set loading state to false in case of an error
+    }
   };
 
   return (
